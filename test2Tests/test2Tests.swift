8668fa73efc314dfaabe0f42c5e59dbaefcdc338
shawn8989 <shunathon@icloud.com>
2025-11-18 13:16:40 -0800

    Initial Commit

---
+++ test2Tests/test2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  test2Tests.swift
+//  test2Tests
+//
+//  Created by shunathon Owens on 11/18/25.
+//
+
+import Testing
+@testable import test2
+
+struct test2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
